345e29aadcbee3754562b1ed1fcdc624ea725980
eddienorte <eddienorte@gmail.com>
2017-12-02 14:55:04 -0200

    Add files via upload

--- data/Praias_4.js
+++ data/Praias_4.js
@@ -1,12 +1,1 @@
-var json_Praias_4 = {
-"type": "FeatureCollection",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-                                                                                
-"features": [
-{ "type": "Feature", "properties": { "Name:": "Praia do Mar Grosso", "imagem:": "Imagens\/mar_grosse.jpg" }, "geometry": { "type": "Point", "coordinates": [ -51.993213099999942, -32.055446999999319, 0.0 ] } },
-{ "type": "Feature", "properties": { "Name:": "Barranco", "imagem:": "Imagens\/barranco.jpg" }, "geometry": { "type": "Point", "coordinates": [ -52.039921499999949, -31.973941299999346, 0.0 ] } },
-{ "type": "Feature", "properties": { "Name:": "Barrinha do Estreito", "imagem:": "Imagens\/img-barrinha-02.jpg" }, "geometry": { "type": "Point", "coordinates": [ -51.731055038999955, -31.842709265999339, 0.0 ] } },
-{ "type": "Feature", "properties": { "Name:": "Molhes da Barra", "imagem:": "Imagens\/molhes_barra.jpg" }, "geometry": { "type": "Point", "coordinates": [ -52.079141967999952, -32.149113683999239, 0.0 ] } },
-{ "type": "Feature", "properties": { "Name:": "Farol da Barra", "imagem:": "Imagens\/farol.jpg" }, "geometry": { "type": "Point", "coordinates": [ -52.07684834499999, -32.117866617999269, 0.0 ] } }
-]
-}
+var json_Praias_4 = {"type":"FeatureCollection","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Name:":"Praia do Mar Grosso","imagem:":"Imagens\/mar_grosse.jpg"},"geometry":{"type":"Point","coordinates":[-51.993213099999942,-32.055446999999319,0.0]}},{"type":"Feature","properties":{"Name:":"Barranco","imagem:":"Imagens\/barranco.jpg"},"geometry":{"type":"Point","coordinates":[-52.039921499999949,-31.973941299999346,0.0]}},{"type":"Feature","properties":{"Name:":"Barrinha do Estreito","imagem:":"Imagens\/img-barrinha-02.jpg"},"geometry":{"type":"Point","coordinates":[-51.731055038999955,-31.842709265999339,0.0]}},{"type":"Feature","properties":{"Name:":"Molhes da Barra","imagem:":"Imagens\/molhes_barra.jpg"},"geometry":{"type":"Point","coordinates":[-52.079141967999952,-32.149113683999239,0.0]}},{"type":"Feature","properties":{"Name:":"Farol da Barra","imagem:":"Imagens\/farol.jpg"},"geometry":{"type":"Point","coordinates":[-52.07684834499999,-32.117866617999269,0.0]}}]}
